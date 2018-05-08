6ffffd4d0e9b9ba68f0ee1ac51af13f73da4c1cd
admin <admin@USCS-Mac123.local>
2018-05-08 00:36:22 -0700

    initial commit

--- routes/disaster.js
+++ routes/disaster.js
@@ -5,10 +5,31 @@
 var express = require('express');
 var router = express.Router();
 var Disaster = require('../models/disaster');
-router.get('/', function (req, res, next) {
+router.get('/year', function (req, res, next) {
     Disaster.aggregate({ $group: {
-            _id: "$year", count:{$sum : 1}
+            year: "$year", count:{$sum : 1}
         }})
+        .exec(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        });
+});
+router.get('/item', function (req, res, next) {
+    Disaster.aggregate([
+        { $match: {
+            item: 2017
+        }},
+      { $group: {
+            item: "$item", count:{$sum : 1}
+        }}])
         .exec(function (err, result) {
             if (err) {
                 return res.status(500).json({
